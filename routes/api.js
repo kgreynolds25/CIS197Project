f90a6d6abb7f7e701d53f8199bcbbdd9f33ebe00
kgreynolds25 <49466124+kgreynolds25@users.noreply.github.com>
2019-04-15 22:45:59 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,11 @@
-var express = require('express')
-var router = express.Router();
-var Payment = require('../models/payment.js')
+const express = require('express')
+const router = express.Router();
+const Payment = require('../models/payment.js')
+const User = require('../models/user.js')
 
-//might need to fix the res.json() part. before I had result.paymentAmount but
-// I don't think that was right
 router.get('/getPayments', function (req, res, next) {
-  Payment.find({}, function (err, result) {
-    if (err) next(err)
+  ps = Payment.find({}, function (err, result) {
+    if (err) return next(err)
     res.json(result.paymentAmount)
   })
 })
@@ -14,11 +13,42 @@ router.get('/getPayments', function (req, res, next) {
 router.post('/addPayment', function (req, res, next) {
   const { paymentAmount, receiver } = req.body;
   const author = req.session.user.username;
-  const p = new Payment({ paymentAmount, author, receiver })
-  p.save(function (err, result) {
-    if (err) next(err)
-    res.json({ status: 'OK'})
-  })
+  const money = req.session.user.money;
+  if (money >= paymentAmount) {
+    User.findOne({ username: author }, function (err, u) {
+      if (err) return next(err);
+      u.money = Number(money) - Number(paymentAmount);
+      u.save(function (saveErr, result) {
+        if (saveErr) return next(saveErr);
+        req.session.user = result;
+        const p = new Payment({ paymentAmount, author, receiver });
+        p.save(function (err, result) {
+          if (err) return next(err);
+          res.json(u);
+        })
+      });
+    });
+  } else {
+    next(new Error('you do not have enough money in your account'));
+  }
+})
+
+router.post('/addMoney', function (req, res, next) {
+  const moneyAdded = req.body.moneyAdded;
+  const { username, money } = req.session.user;
+  if (moneyAdded >= 0) {
+    User.findOne({ username }, function (err, u) {
+      if (err) return next(err);
+      u.money = Number(money) + Number(moneyAdded);
+      u.save(function (saveErr, result) {
+        if (saveErr) return next(saveErr);
+        req.session.user = result;
+        res.json({status: 'OK'});
+      });
+    });
+  } else {
+    next(new Error('invalid amount'));
+  }
 })
 
 module.exports = router;
